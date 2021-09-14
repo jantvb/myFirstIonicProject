e7ed0f7c9bad78bb1d404a6c1eac077b75ca208b
Jose Vicente <jantvb@gmail.com>
2021-09-14 19:18:16 -0400

    Using Capacitor

---
+++ android/app/src/main/java/com/jose/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.jose.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'com.jose.app',
   appName: 'myFirstIonicProject',
   webDir: 'www',
   bundledWebRuntime: false
